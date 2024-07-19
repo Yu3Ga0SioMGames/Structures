c8aa3c4ef15106b1a389fd03da8527d4b9c69390
Yu3Ga0SioMGames <144995584+Yu3Ga0SioMGames@users.noreply.github.com>
2024-07-19 23:03:00 +0300

    origin/main (#5)
    * Implemented a simple stack.
    
    * Fix any bugs and added new 2 testes.
    
    * "stack.c" Has been fixed...
    
    * Fix any errors.
    
    * Add "container_error" and fix him.
    
    * Add directory "stack".
    
    * Add test for stack.
    
    * Add stack_test.

--- main.c
+++ main.c
@@ -1,30 +1,6 @@
-#include "stack/stack.h"
+#include "stack/stack_test/stack_test.h"
 
 int main()
 {
-    Stack *stack;
-    stack = NULL;
-
-    printf("%ld\n", stack_pop(stack));
-    printf("\n%ld\n\n", stack_peek(stack));
-
-    stack = create_stack();
-    int64_t initial_size = 10;
-
-
-    stack_push(stack, 1);
-    printf("%ld\n", stack_peek(stack));
-    printf("\n%ld\n", stack_pop(stack));
-    printf("\n%ld\n\n", stack_peek(stack));
-    for(int64_t i = 0; i < initial_size; ++i) {
-        stack_push(stack, i);
-    }
-    stack_push(stack, 87);
-    print_stack(stack);
-    printf("\n%ld\n\n", stack_pop(stack));
-    print_stack(stack);
-    printf("\n%ld\n\n", stack_peek(stack));
-
-    free_stack(stack);
-    return 0;
+    return stack_test();
 }
---
+++ stack/stack_test/stack_test.h
@@ -0,0 +1,8 @@
+#ifndef STACK_TEST_H_INCLUDED
+#define STACK_TEST_H_INCLUDED
+
+#include "../stack/stack.h"
+
+int stack_test();
+
+#endif
