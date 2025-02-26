0ba1c94929885d398744f72a83d53520d22b4a37
Yu3Ga0SioMGames <144995584+Yu3Ga0SioMGames@users.noreply.github.com>
2025-02-26 23:06:10 +0300

    Reading and writing does not work, code does not work and not enough sorting. At this moment...

--- main.c
+++ main.c
@@ -1,6 +1,42 @@
-#include "tree/test/test.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-	return remove_test();
+	int count = 100;
+	char filename[25];
+
+	sprintf(filename, "%d_data_%d.txt", count, count);
+
+	FILE *file_read = fopen(filename, "r");
+	if(file_read == NULL) {
+		return 1;
+	}
+
+	int arr[count];
+
+	size_t readed_elements = fread(arr, sizeof(int), count, file_read);
+	fclose(file_read);
+	if(readed_elements != count) {
+		return 1;
+	} else {
+		for(int i = 0; i < count; ++i) {
+			printf("%d ", arr[i]);
+		}
+	}
+
+	// ArraySort(arr);
+
+	FILE *file_write = fopen("result.txt", "w+");
+	if(file_write == NULL) {
+		return 1;
+	}
+
+	size_t writer_elements = fwrite(arr, sizeof(int), count, file_write);
+	fclose(file_write);
+	if(writer_elements != count) {
+		return 1;
+	}
+
+	return 0;
 }
