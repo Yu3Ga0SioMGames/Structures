b7aef465a7d372d8d5162a939e03c44d38a89011
Yu3Ga0SioMGames <144995584+Yu3Ga0SioMGames@users.noreply.github.com>
2025-03-02 14:58:22 +0300

    Just tidying up... (#9)
    * Reading and writing does not work, code does not work and not enough sorting. At this moment...
    
    * Fix file read and write --- code work. First sorting add - this is bubble and optimize bubble sort.
    
    * Add new sorting: selection and insertion.
    
    * Add MergeSort.
    
    * Add new quick sorting.
    
    * Just tidying up...

---
+++ sort_test/create_data_script.h
@@ -0,0 +1,11 @@
+#ifndef CREATE_DATA_SCRIPT_H_INCLUDED
+#define CREATE_DATA_SCRIPT_H_INCLUDED
+
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+void create_data();
+
+#endif
---
+++ sortings/test/test.h
@@ -0,0 +1,12 @@
+#ifndef TEST_H_INCLUDED
+#define TEST_H_INCLUDED
+
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <sorting.h>
+
+int test_sort();
+
+#endif
